b4d539acb8a78edda5dc06aa6bf82e8c9c40eb40
matildaIE <matilda.rutherford@ie.com.au>
2020-02-02 21:14:46 +1100

    typo

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -18,6 +18,12 @@ const Form = () => {
    const [message, setMessage] = React.useState('')
    const [errors, setError] = React.useState(errorInitial)
 
+   const encode = (data) => {
+      return Object.keys(data)
+         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+         .join("&");
+   }
+
    const submitForm = async (event) => {
       event.preventDefault()
       await validateFields()
@@ -38,137 +44,139 @@ const Form = () => {
             .catch(error => alert(error))
 
       }
-      const handleChange = (field, value) => {
-         if (field === 'name') {
-            setName(value)
-         } else if (field === 'email') {
-            setEmail(value)
-         } else if (field === 'subject') {
-            setSubject(value)
-         } else if (field === 'message') {
-            setMessage(value)
-         }
-      }
-
-      const validateFields = () => {
-         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-         if (name === '') {
-            setError({
-               ...errors,
-               name: true
-            })
-         } else {
-            setError({
-               ...errors,
-               name: false
-            })
-         }
-
-         if (emailRegex.test(String(email).toLowerCase()) === false) {
-            setError({
-               ...errors,
-               email: true
-            })
-         } else {
-            setError({
-               ...errors,
-               email: false
-            })
-         }
-
-         if (subject === '') {
-            setError({
-               ...errors,
-               subject: true
-            })
-         } else {
-            setError({
-               ...errors,
-               subject: false
-            })
-         }
-
-         if (message === '') {
-            setError({
-               ...errors,
-               message: true
-            })
-         } else {
-            setError({
-               ...errors,
-               message: false
-            })
-         }
-      }
-
-      return (
-         <div className='form-wrapper'>
-            <div className="form-text">
-               <div className='form-text-wrapper'>
-                  <h2>First coffee free.</h2>
-                  <p>Ideas are what drive new business. Creativity is the food for ideas.</p>
-                  <p>The first coffee is <span>free.</span></p>
-                  <p>Contact us about your requirements and we will put you in contact with the team member that is best suited to answer your needs. We’ll have a chat for the cost of a coffee.</p>
-               </div>
-            </div>
-            <form onSubmit={submitForm} className='form' method="POST" data-netlify="true" netlify>
-               <input type="hidden" name='form-name' value='contact' />
-               <TextField
-                  id='name'
-                  label='Name'
-                  value={name}
-                  onChange={(text) => handleChange('name', text.target.value)}
-                  required={true}
-                  helperText={!!errors.name ? "Please enter your name" : null}
-                  fullWidth={true}
-                  className='form-input'
-               />
-               <TextField
-                  id='email'
-                  label='Email'
-                  value={email}
-                  onChange={(text) => handleChange('email', text.target.value)}
-                  required={true}
-                  helperText={!!errors.email ? "Please enter your email" : null}
-                  fullWidth={true}
-                  className='form-input'
-               />
-               <TextField
-                  id='subject'
-                  label='Subject'
-                  value={subject}
-                  onChange={(text) => handleChange('subject', text.target.value)}
-                  required={true}
-                  helperText={!!errors.subject ? "Please enter an email subject" : null}
-                  fullWidth={true}
-                  className='form-input'
-               />
-               <TextField
-                  id='message'
-                  label='Message'
-                  value={message}
-                  onChange={(text) => handleChange('message', text.target.value)}
-                  required={true}
-                  helperText={!!errors.message ? "Please enter a message" : null}
-                  multiline
-                  rows={6}
-                  fullWidth={true}
-                  className='form-input'
-               />
-               <Button
-                  size='large'
-                  variant='contained'
-                  onClick={submitForm}
-                  type='submit'
-                  className='form-button'
-               >
-                  Send
-            </Button>
-            </form>
-
-         </div>
-      )
    }
 
-   export default Form
+   const handleChange = (field, value) => {
+      if (field === 'name') {
+         setName(value)
+      } else if (field === 'email') {
+         setEmail(value)
+      } else if (field === 'subject') {
+         setSubject(value)
+      } else if (field === 'message') {
+         setMessage(value)
+      }
+   }
+
+   const validateFields = () => {
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (name === '') {
+         setError({
+            ...errors,
+            name: true
+         })
+      } else {
+         setError({
+            ...errors,
+            name: false
+         })
+      }
+
+      if (emailRegex.test(String(email).toLowerCase()) === false) {
+         setError({
+            ...errors,
+            email: true
+         })
+      } else {
+         setError({
+            ...errors,
+            email: false
+         })
+      }
+
+      if (subject === '') {
+         setError({
+            ...errors,
+            subject: true
+         })
+      } else {
+         setError({
+            ...errors,
+            subject: false
+         })
+      }
+
+      if (message === '') {
+         setError({
+            ...errors,
+            message: true
+         })
+      } else {
+         setError({
+            ...errors,
+            message: false
+         })
+      }
+   }
+
+   return (
+      <div className='form-wrapper'>
+         <div className="form-text">
+            <div className='form-text-wrapper'>
+               <h2>First coffee free.</h2>
+               <p>Ideas are what drive new business. Creativity is the food for ideas.</p>
+               <p>The first coffee is <span>free.</span></p>
+               <p>Contact us about your requirements and we will put you in contact with the team member that is best suited to answer your needs. We’ll have a chat for the cost of a coffee.</p>
+            </div>
+         </div>
+         <form onSubmit={submitForm} className='form' method="POST" data-netlify="true" netlify>
+            <input type="hidden" name='form-name' value='contact' />
+            <TextField
+               id='name'
+               label='Name'
+               value={name}
+               onChange={(text) => handleChange('name', text.target.value)}
+               required={true}
+               helperText={!!errors.name ? "Please enter your name" : null}
+               fullWidth={true}
+               className='form-input'
+            />
+            <TextField
+               id='email'
+               label='Email'
+               value={email}
+               onChange={(text) => handleChange('email', text.target.value)}
+               required={true}
+               helperText={!!errors.email ? "Please enter your email" : null}
+               fullWidth={true}
+               className='form-input'
+            />
+            <TextField
+               id='subject'
+               label='Subject'
+               value={subject}
+               onChange={(text) => handleChange('subject', text.target.value)}
+               required={true}
+               helperText={!!errors.subject ? "Please enter an email subject" : null}
+               fullWidth={true}
+               className='form-input'
+            />
+            <TextField
+               id='message'
+               label='Message'
+               value={message}
+               onChange={(text) => handleChange('message', text.target.value)}
+               required={true}
+               helperText={!!errors.message ? "Please enter a message" : null}
+               multiline
+               rows={6}
+               fullWidth={true}
+               className='form-input'
+            />
+            <Button
+               size='large'
+               variant='contained'
+               onClick={submitForm}
+               type='submit'
+               className='form-button'
+            >
+               Send
+            </Button>
+         </form>
+
+      </div>
+   )
+}
+
+export default Form
